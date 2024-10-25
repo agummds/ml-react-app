93635c685b19476d04ec1168c70d1dabd392cb86
unknown <medisaagum@.gmail.com>
2024-10-25 17:33:04 +0700

    menambahakan beberapa style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
-
   return (
-    <>
-    
-    </>
+    <div className='flex flex-col p-4 max-w-[1000px] mx-auto w-full'>
+      <section className='min-h-screen flex flex-col'>
+        <header className='flex item-center justify-between gp-6'>
+        <h1><span className='text-red-400'>Trans</span>cribe</h1>
+        <button className='flex item-center gap-1'>
+          <p>New</p>
+          <i className="fa-solid fa-plus"></i>
+        </button>
+        </header>
+        <main className='flex-1'>
+
+        </main>
+      </section>
+    <h1 className='text-green-400'>Agum Medisa</h1>
+    <footer>
+
+    </footer>
+    </div>
   )
 }
 
