a885dd8092da617444b265e510c430a198d4b936
unknown <medisaagum@.gmail.com>
2024-10-26 12:23:25 +0700

    pemambahan component audio dan style

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -4,23 +4,24 @@ export default function FileDisplay(props){
     const {handleAudioReset, file, audioStream} = props 
     return(
             <main className='flex-1 p-4 flex flex-col justify-center 
-            gap-2 text-center sm:gap-3 md:gap-4 justify-center pb-20 w-fit max-w-full mx-auto'>
+            gap-2 text-center sm:gap-3 md:gap-4 justify-center pb-20 w-72 max-w-full mx-auto'>
                 <h1 className="font-semibold text-3xl sm:text-4xl md:text-5xl "><span 
                 className="text-red-400 bold">Your </span>File</h1>
 
-                <div className="mx-auto flex flex-col text-center mb-2">
-                    <h3 className="font-semibold">Name:</h3>
-                    <p>{file.name}</p>
+                <div className="mx-auto flex flex-col text-center my-2">
+                    <h3 className="font-semibold">Name Audio:</h3>
+                    <p>{file? file?.name : 'Costum Audio'}</p>
                 </div>
 
                 <div className="flex items-center justify-between gap-4">
-                    <button className="text-slate-400 ">
+                    <button onClick={handleAudioReset} className="text-slate-400 hover:text-red-200 duration">
                         Reset
                     </button>
-                    <button className="specialBtn px-4 py-2 rounded-lg text-red-400">
+                    <button className="specialBtn px-4 p-2 rounded-lg text-red-400 flex items-center gap-1">
                         <p>
                         Transcribe
                         </p>
+                        <i class="fa-solid fa-pencil"></i>
                     </button>
                 </div>
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,73 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 export default function HomePage(props){
 
     const {setAudioStream, setFile} = props
+    const [recordingStatus, setRecordingStatus] = useState('inactive')
+    const [audioChunks, setAudioChunks] = useState([])
+    const [duration, setDuration] = useState(0)
 
+    const mediaRecorder = useRef(null)
+
+    const mimeType = 'audio/webm'
+
+    async function startRecording() {
+        let tempStream
+        console.log ('Start recording')
+        try {
+            const streamData = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video : false
+            })
+            tempStream = streamData
+
+        }catch (err){
+            console.log(err.message)
+            return
+        }
+
+        setRecordingStatus('recording')
+
+        //Create new Media Recorder
+        const media = new  MediaRecorder(tempStream, {type: mimeType})
+
+        mediaRecorder.current = media
+        mediaRecorder.current.start()
+        let localAudioChunks = []
+        mediaRecorder.current.ondataavailable = (event) =>{
+
+            if(typeof event.data === 'undefined'){return}
+            if(event.data.size === 0){return}
+            localAudioChunks.push(event.data)
+
+        }
+        setAudioChunks(localAudioChunks)
+        
+    }
+
+    async function stopRecording() {
+
+        setRecordingStatus('inactive')
+        console.log('Stop Recording')
+
+        mediaRecorder.current.stop()
+        mediaRecorder.current.onstop = () =>{
+            const audioBlob = new Blob (audioChunks, {type: mimeType})
+            setAudioStream(audioBlob)
+            setAudioChunks([])
+            setDuration(0)
+        }
+    }
+
+    useEffect(() => {
+        if (recordingStatus === 'inactive') {return}
+
+        const interval = setInterval(() => {
+            setDuration(curr => curr +1)
+        }, 1000)
+
+        return () => clearInterval(interval)
+    })
 
     return(
         <main className='flex-1 p-4 flex flex-col justify-center gap-2 text-center sm:gap-3 md:gap-4 justify-center pb-80'>
@@ -11,11 +75,18 @@ export default function HomePage(props){
             <h3 className="font-semibold md:text-lg"> Record <span 
             className="text-red-400">&rarr;</span> Transcribe<span 
             className="text-red-400">&rarr;</span> Translate</h3>
-            <button className="flex items-center text-base justify-between gap-5 mx-auto w-72 max-w-full my-5 specialBtn px-3 py-2 rounded-xl">
-                <p className="text-red-400">Record</p>
-                <i className="fa-solid fa-microphone-lines"></i>
+            <button onClick={recordingStatus === 'recording' ? stopRecording : startRecording} className="flex items-center text-base justify-between gap-5 mx-auto w-72 max-w-full my-5 specialBtn px-3 py-2 rounded-xl">
+                <p className="text-red-400">{ recordingStatus === 'inactive'? 'Record' :  `Stop recording`}</p>
+                <div className="flex items-center gap-2">  
+                    {duration && (
+                        <p className="text-sm">
+                            {duration}s
+                        </p>
+                    )}
+                <i className={"fa-solid duration-200 fa-microphone-lines" + (recordingStatus === 'recording' ? 'text-rose-400' : "")}></i>
+                </div>
             </button>
-            <p className="text-base">Or  <label className="text-red-500 cursor-pointer hover:text-red-500 duration-100"
+            <p className="text-base">Or  <label className="text-red-500 cursor-pointer hover:text-red-300 duration-200"
             >Upload <input onChange={(e) => {
                 const tempFile = e.target.files[0] 
                 setFile(tempFile)
