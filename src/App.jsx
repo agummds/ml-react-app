8e5348ee968ed8a50cbed791620e4c1c39f522ab
unknown <medisaagum@.gmail.com>
2024-10-26 14:07:26 +0700

    memberpaiki error Transcribing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import HomePage from './components/HomePage'
 import Header from './components/Header'
 import FileDisplay from './components/FileDisplay'
-import Transcribe from './components/Transcribe'
+import Transcribing from './components/Transcribing'
 
 
 function App() {
@@ -11,6 +11,9 @@ function App() {
 
   const isAudioAvailable = file || audioStream
 
+  const [output, setOutput] = useState(null)
+  const [loading, setLoading] = useState(true )
+ 
   function handleAudioReset(){
     setFile(null)
     setAudioStream(null)
@@ -24,10 +27,15 @@ function App() {
     <div className='flex flex-col p-4 max-w-[1000px] mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
         <Header/>
-        {isAudioAvailable ? (
+        {output ? (
+          <Information/>
+        ) : loading ? (
+          <Transcribing/>
+        ) : isAudioAvailable ? (
           <FileDisplay handleAudioReset = {handleAudioReset} file={file} audioStream= {setAudioStream} />
-        ) : (
-        <HomePage setFile={setFile} setAudioStream={setAudioStream}/>)}
+        ) :
+          <HomePage setFile={setFile} setAudioStream={setAudioStream}/>
+      }
       </section>
     <h1 className='text-green-400'>Agum Medisa</h1>
     <footer>
