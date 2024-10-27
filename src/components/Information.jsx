09abe20ba3cb5b7b22300701d22cedf82a69e60c
unknown <medisaagum@.gmail.com>
2024-10-27 19:09:49 +0700

    menambahkan style pada Information.jsx

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react"
 import Transcription from "./Transcription"
 import Translation from "./Translation"
 
-export default function Information(){
+export default function Information(props){
+    const {output} = props
     const [tab, setTab] = useState('Trasnkrip')
+    console.log (output)
     return(
     <main className='flex-1 p-4 flex flex-col justify-center 
     gap-2 text-center sm:gap-3 md:gap-4 justify-center pb-20 max-w-prose w-full mx-auto'>
@@ -15,20 +17,30 @@ export default function Information(){
         <button onClick={()=>{
             setTab('Transkrip')
         }} className={"px-10 duration-200 py-2 font-medium text-red-400" + (tab === 'Transkrip' ? 
-            ' bg-red-400 text-white' : 'text-red-400 hover:text-red-500')}>Transkrip
+            ' bg-red-200 text-white' : 'text-red-400 hover:text-red-500')}>Transkrip
             </button>
         <button onClick={()=>{
             setTab('Terjemahan')
         }} className={"px-10 duration-200 py-2 font-medium text-red-400"+ (tab === 'Terjemahan' ? 
-            ' bg-red-400 text-white' : 'text-red-400 hover:text-red-500')}>
+            ' bg-red-200 text-white' : 'text-red-400 hover:text-red-500')}>
                 Terjemahan
         </button>
         </div>
+        <div className="my-5 flex flex-col">
         {tab === 'Transkrip'?(
-            <Transcription/>
+            <Transcription {...props} />
         )   : (
-            <Translation/>
+            <Translation {...props} />
         )}
+        </div>
+        <div className="flex items-center gap-10 mx-auto">
+            <button title="Copy" className="bg-transparent hover:text-red-100 duration-300 text-red-600 px-2 aspec-square grid place-itmes-center rounded">
+            <i className="fa-solid fa-clone"></i>
+            </button>
+            <button title="Download" className="bg-transparent hover:text-red-100 duration-300 text-red-600 px-2 aspec-square grid place-itmes-center rounded">
+            <i className="fa-solid fa-download"></i>
+            </button>
+        </div>
         </main>
 
     )
--- src/components/Transcription.jsx
+++ src/components/Transcription.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react"
 
-export default function Transcription(){
+export default function Transcription(props){
+    const {output} = props
+    console.log(output)
+    const finalText = output.map(val => val.text)
     return(
         <div>
-            Hasil Transkrip
+            {finalText}
         </div>
     )
 }
